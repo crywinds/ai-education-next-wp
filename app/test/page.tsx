0b34442b0bf740aa2979a33b45c0b4dc61761fe6
royal69hk <towing11@hotmail.com>
2025-12-30 16:37:29 +0800

    Add test page for debugging

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '2rem', background: 'white', minHeight: '100vh' }}>
+      <h1 style={{ color: 'black', fontSize: '2rem' }}>測試頁面</h1>
+      <p style={{ color: 'black' }}>如果您能看到這個頁面，說明基本渲染正常。</p>
+    </div>
+  )
+}
+
